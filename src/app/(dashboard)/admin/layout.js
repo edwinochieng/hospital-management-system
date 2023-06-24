93e64ba349e7ee893f828602659c69bc529a0c60
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-24 22:51:13 +0300

    admin dashboard layout

---
+++ src/app/(dashboard)/admin/layout.js
@@ -0,0 +1,19 @@
+import SideBar from "@/components/SideBar";
+
+const sidebarItems = [
+  { title: "Overview", link: "/admin" },
+  { title: "Doctor List", link: "/admin/doctors" },
+  { title: "Patient List", link: "/admin/patients" },
+  { title: "Staff List", link: "/admin/staff" },
+  { title: "Add New Doctor", link: "/admin/add-doctor" },
+  { title: "Add New Staff", link: "/admin/add-staff" },
+];
+
+export default function AdminLayout({ children }) {
+  return (
+    <div className='flex flex-row'>
+      <SideBar items={sidebarItems} />
+      <div className='bg-gray-100 w-full h-screen'>{children}</div>
+    </div>
+  );
+}
