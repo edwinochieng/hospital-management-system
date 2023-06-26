62176691fed0f91492ddd0b6dc663121e5b67db4
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-26 12:42:42 +0300

    add sign in logic

--- src/app/(auth)/auth/admin/page.js
+++ src/app/(auth)/auth/admin/page.js
@@ -1,22 +1,49 @@
 "use client";
 
+import { signIn } from "next-auth/react";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { getError } from "../../../../../utils/error";
+import { useRouter } from "next/navigation";
 
 export default function AdminLogin() {
+  const router = useRouter();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
+  const submitHandler = async ({ username, password }) => {
+    try {
+      const result = await signIn("credentials", {
+        redirect: false,
+        username,
+        password,
+      });
+      if (result.error) {
+        toast.error(result.error);
+      } else {
+        router.push("/admin");
+        toast.success("Logged in");
+      }
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
+
   return (
     <div className='max-w-[700px] mx-auto'>
-      <form className='bg-white space-y-4 rounded-xl py-8 px-8 shadow-2xl'>
+      <form
+        className='bg-white space-y-4 rounded-xl py-8 px-8 shadow-2xl'
+        onSubmit={handleSubmit(submitHandler)}
+      >
         <p className='text-lg font-medium'>Administrator Login</p>
 
         <div>
-          <label htmlFor='email' className='text-sm font-medium'>
+          <label htmlFor='username' className='text-sm font-medium'>
             Username
           </label>
 
@@ -25,8 +52,8 @@ export default function AdminLogin() {
               {...register("username", {
                 required: "Please enter username",
               })}
-              type='email'
-              id='emai'
+              type='text'
+              id='username'
               className='w-full rounded-lg border border-gray-200 p-4 pr-12 text-sm focus:outline-indigo-500'
               placeholder='Enter username'
             />
