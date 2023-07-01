a5e4fb628b126c6dd1cf6a4f1e757c59ba5c725a
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-07-01 17:51:51 +0300

    doctor dashboard layout

---
+++ src/app/(dashboard)/doctor/layout.js
@@ -0,0 +1,16 @@
+import SideBar from "@/components/SideBar";
+
+const sidebarItems = [
+  { title: "Overview", link: "/doctor" },
+  { title: "Appointments", link: "/doctor/appointments" },
+  { title: "My Patients", link: "/doctor/patients" },
+];
+
+export default function DoctorDashBoardLayout({ children }) {
+  return (
+    <div className='flex flex-row'>
+      <SideBar items={sidebarItems} />
+      <div className='bg-gray-100 w-full h-screen pt-24'>{children}</div>
+    </div>
+  );
+}
