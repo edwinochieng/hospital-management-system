e88004eee8d204000ad81051b1269eef8b667aa0
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-07-01 17:50:58 +0300

    add sign out button

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,12 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { signOut } from "next-auth/react";
 
 export default function SideBar({ items }) {
   const pathname = usePathname();
+
   return (
     <div className='h-screen bg-white w-[350px]'>
       <nav className='p-3'>
@@ -24,6 +26,12 @@ export default function SideBar({ items }) {
           })}
         </ul>
       </nav>
+      <div
+        className='m-3 font-medium text-gray-800 p-2 rounded-md hover:bg-gray-200 cursor-pointer'
+        onClick={() => signOut({ callbackUrl: "/" })}
+      >
+        Log Out
+      </div>
     </div>
   );
 }
