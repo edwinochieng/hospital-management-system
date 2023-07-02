2007d231ef4a420a1ecebdf217e87e9f76b285db
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-07-02 18:00:49 +0300

    remove register staff

--- src/app/(dashboard)/admin/layout.js
+++ src/app/(dashboard)/admin/layout.js
@@ -4,9 +4,8 @@ const sidebarItems = [
   { title: "Overview", link: "/admin" },
   { title: "Doctor List", link: "/admin/doctors" },
   { title: "Patient List", link: "/admin/patients" },
-  { title: "Staff List", link: "/admin/staff" },
   { title: "Add New Doctor", link: "/admin/add-doctor" },
-  { title: "Add New Staff", link: "/admin/add-staff" },
+
 ];
 
 export default function AdminLayout({ children }) {
