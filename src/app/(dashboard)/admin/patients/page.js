0c776b0affcc91636d9e8b87b067d66fe967b4c3
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-07-03 23:29:20 +0300

    display patients list

--- src/app/(dashboard)/admin/patients/page.js
+++ src/app/(dashboard)/admin/patients/page.js
@@ -1,5 +1,37 @@
 import React from "react";
 
-export default function PatientsList() {
-  return <div>PatientsList</div>;
+const getPatients = async () => {
+  const res = await fetch(`${process.env.BASE_URL}/api/patients`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+export default async function PatientsList() {
+  const data = await getPatients();
+  return (
+    <div className='max-w-[1000px] mx-auto'>
+      <table className='w-full bg-white border border-gray-200 rounded shadow'>
+        <thead>
+          <tr className='bg-gray-100'>
+            <th className='px-4 py-2 font-semibold text-left'>Name</th>
+            <th className='px-4 py-2 font-semibold text-left'>Email</th>
+            <th className='px-4 py-2 font-semibold text-left'>Gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((patient) => (
+            <tr key={patient.id} className='border-b border-gray-200'>
+              <td className='px-4 py-2'>{patient.name}</td>
+              <td className='px-4 py-2'>{patient.email}</td>
+              <td className='px-4 py-2'>{patient.gender}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
