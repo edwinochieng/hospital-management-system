3877aceb77375a1c94877254decc7e8a53a37979
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-07-03 14:45:34 +0300

    fetch patients from db

--- src/app/(dashboard)/receptionist/patients/page.js
+++ src/app/(dashboard)/receptionist/patients/page.js
@@ -1,63 +1,18 @@
-"use client";
+import SearchPatients from "@/components/SearchPatient";
 import React from "react";
-import { useForm } from "react-hook-form";
-import { toast } from "react-hot-toast";
-import { getError } from "../../../../../utils/error";
 
-export default function SearchPatients() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+const getPatients = async () => {
+  const res = await fetch(`${process.env.BASE_URL}/api/patients`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-  const submitHandler = async ({ name }) => {
-    try {
-      await axios.post("/api/receptionist/registerPatient", {
-        name,
-      });
-      toast.success("Patient registered successfully");
-    } catch (err) {
-      toast.error(getError(err));
-    }
-  };
-  return (
-    <div>
-      <div className='max-w-[700px] mx-auto'>
-        <form
-          className='bg-white rounded-xl p-8 shadow-2xl'
-          onSubmit={handleSubmit(submitHandler)}
-        >
-          <p className='text-lg font-medium'>Search Patient</p>
-          <div className='flex flex-row items-center space-x-4 w-full'>
-            <div className='relative w-full'>
-              <input
-                {...register("name", {
-                  required: "Please enter name",
-                })}
-                type='name'
-                id='name'
-                className='w-full rounded-lg border border-gray-200 p-4 pr-12 text-sm focus:outline-indigo-500'
-                placeholder='Enter name'
-              />
-            </div>
+  return res.json();
+};
+export default async function Patients() {
+  const data = await getPatients();
 
-            <div className='w-[100px]'>
-              <button
-                type='submit'
-                className='w-full rounded-lg bg-indigo-500 p-4 text-sm font-medium text-white'
-              >
-                Search
-              </button>
-            </div>
-          </div>
-          {errors.name && (
-            <span className='text-red-500 pt-1 text-sm'>
-              {errors.name.message}
-            </span>
-          )}
-        </form>
-      </div>
-    </div>
-  );
+  return <SearchPatients results={data} />;
 }
