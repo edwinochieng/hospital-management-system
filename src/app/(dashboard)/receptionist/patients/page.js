705a779e0d8661a1379d033c047a6ffd99ddc316
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-07-03 15:42:25 +0300

    fetch doctors from db

--- src/app/(dashboard)/receptionist/patients/page.js
+++ src/app/(dashboard)/receptionist/patients/page.js
@@ -11,8 +11,20 @@ const getPatients = async () => {
 
   return res.json();
 };
+const fetchDoctors = async () => {
+  const res = await fetch(`${process.env.BASE_URL}/api/admin/getDoctorList`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
 export default async function Patients() {
   const data = await getPatients();
+  const doctors = await fetchDoctors();
 
-  return <SearchPatients results={data} />;
+  return <SearchPatients results={data} doctors={doctors} />;
 }
