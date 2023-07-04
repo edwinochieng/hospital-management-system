07a6458b929adb71f9556b7f81eca213d13ddaa1
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-07-04 20:14:47 +0300

    fetch appointments

--- src/app/(dashboard)/doctor/page.js
+++ src/app/(dashboard)/doctor/page.js
@@ -1,5 +1,41 @@
+import { getServerSession } from "next-auth";
+import Link from "next/link";
 import React from "react";
 
-export default function DoctorDashboard() {
-  return <div>DoctorDashBoard</div>;
+const fetchAppointments = async (email) => {
+  const res = await fetch(
+    `${process.env.BASE_URL}/api/doctor/getAppointments/${email}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+export default async function DoctorDashboard() {
+  const session = await getServerSession();
+  const email = session.user?.email;
+  const appointments = await fetchAppointments(email);
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <div className='text-3xl font-semibold text-gray-700'>
+        <p>Dr. {session.user?.name}</p>
+      </div>
+      <div className='flex flex-row space-x-8 mt-10'>
+        <div>
+          <Link
+            href='/doctor/appointments'
+            className='bg-white rounded-xl text-lg w-[300px] py-12 shadow-2xl flex flex-col items-center '
+          >
+            <p>Appointments</p>
+            <p className='text-5xl mt-1'>{appointments.length}</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/app/api/doctor/getAppointments/[slug]/route.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../../../../prisma/client";
+
+export async function GET({ params }) {
+  const email = params.slug;
+
+  try {
+    const doctor = await prisma.doctor.findUnique({ where: { email: email } });
+
+    const appointments = await prisma.appointment.findMany({
+      where: {
+        doctor: {
+          id: doctor.id,
+        },
+      },
+      include: {
+        patient: true,
+      },
+    });
+
+    return NextResponse.json(appointments);
+  } catch (err) {
+    return NextResponse.json({ message: "Server error" });
+  }
+}
